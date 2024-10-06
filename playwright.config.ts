bc8efb8a6b6cb55c8591ef7f0cca9297839cc504
ravikiran.padki <ravikiran.padki@gmail.com>
2024-10-07 07:21:12 +1100

    added tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
-  testMatch: ["**.test.ts"],
+  testMatch: ["**.tests.ts"],
 
   /* Configure projects for major browsers */
   projects: [
